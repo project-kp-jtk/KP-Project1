dfe7d1c6b9550efcdf81a060e522bfa95e10fdb7
virly <nadhiravirly@gmail.com>
2018-07-26 08:45:06 +0700

    template
    template

---
+++ project1/application/controllers/template.php
@@ -0,0 +1,17 @@
+<?php
+
+class Template extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	function index(){
+			$data['content_view'] = "display_kurs.php";
+			$this->load->view('v_template',$data);
+	}
+	function history(){
+			$data['content_view'] = "display_history.php";
+			$this->load->view('v_template',$data);
+	}
+}
---
+++ project1/application/views/display_kurs.php
@@ -0,0 +1,49 @@
+
+  <center><h2>Kurs Mata Uang Terakhir</h2></center>
+
+  <table class="table table-striped">
+    <thead>
+      <div align="center">
+      <tr>
+        <th colspan="1"></th>
+        <th colspan="2"><div align="center"><img class="logo-BI" src="<?php echo base_url()?>assets/img/BI.png"</div></th>
+        <th colspan="2"><div align="center"><img class="logo-HSBC" src="<?php echo base_url()?>assets/img/HSBC.png"</div></th>
+        <th colspan="1"><div align="center"><img class="logo-MAS" src="<?php echo base_url()?>assets/img/MAS.png"</div></th>
+      </tr>
+      <tr>
+        <th>Mata Uang</th>
+        <th>Kurs Jual</th>
+        <th>Kurs Beli</th>
+        <th>Bank Buy</th>
+        <th>Bank Sell</th>
+        <th>Avg. Rate</th>
+      </tr>
+    </div>
+    </thead>
+    <tbody>
+      <tr>
+        <td>IDR</td>
+        <td>0</td>
+        <td>0</td>
+        <td>-</td>
+        <td>-</td>
+        <td>0.009406</td>
+      </tr>
+      <tr>
+        <td>EUR</td>
+        <td>17,054.98</td>
+        <td>16,883.03</td>
+        <td>9.04570</td>
+        <td>9.29570</td>
+        <td>1.5980</td>
+      </tr>
+      <tr>
+        <td>USD</td>
+        <td>14,526.00</td>
+        <td>14,382.00</td>
+        <td>-</td>
+        <td>-</td>
+        <td>1.3611</td>
+      </tr>
+    </tbody>
+  </table>
