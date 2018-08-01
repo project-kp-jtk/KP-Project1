7d5cfb11ad266a46e03ad440ea47941184d87ebd
alpinj <31446041+alpinj@users.noreply.github.com>
2018-08-01 09:11:45 +0700

    query_update

--- project1-nadh/application/models/m_exrate.php
+++ project1-nadh/application/models/m_exrate.php
@@ -8,20 +8,20 @@
         }
 
         function get_data_sourcebased($source_id){
-            $sql_select = "SELECT * FROM exchange_rate WHERE SOURCE='".$source_id."' ORDER BY TANGGAL DESC, MATA_UANG ASC";
+            $sql_select = "SELECT * FROM exchange_rate WHERE SOURCE='".$source_id."' ORDER BY KODE ASC, TANGGAL DESC";
             $result = $this->db->query($sql_select);
             return $result;
+        }
+
+        function insert_data($table, $data){
+            $this->db->insert($table, $data);
         }
 
         function get_data_datebased($source_id){
-            $sql_select = "SELECT * FROM exchange_rate WHERE TANGGAL=(SELECT MAX(TANGGAL) FROM exchange_rate WHERE ID_SOURCE='$source_id')";
+            $sql_select = "SELECT * FROM exchange_rate WHERE DATE(TANGGAL)=(SELECT MAX(DATE(TANGGAL)) FROM (SELECT * FROM exchange_rate WHERE ID_SOURCE=".$source_id.") AS `derived`) AND ID_SOURCE=".$source_id;
             $result = $this->db->query($sql_select);
             return $result;
         }
-
-        function insert_data($table, $data, $source){
-            $this->db->insert($table, $data);
-            }
-        }
-
+    }
+    
 ?>
