efdc8817770b6591256f4b9db254fd49fb24b43c
virly <nadhiravirly@gmail.com>
2018-08-03 14:05:59 +0700

    import view
    benerin import view

--- project1-nadh/application/views/import_view.php
+++ project1-nadh/application/views/import_view.php
@@ -1,17 +1,24 @@
 <h2>Import Data</h2>
 <br>
 <font size="3">
-
-<div class="head">
-<?php echo form_open_multipart('import/import_file'); ?>
-  <div class="form-group">
-    <input type="file" class="form-control-file" name="file" aria-describedby="fileHelp">
-    <small id="fileHelp" class="form-text text-muted">Input file to import.</small>
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-4">
+    </div>
+    <div class="col-sm-5">
+      <?php echo form_open_multipart('import/import_file'); ?>
+        <div class="form-group">
+          <input type="file" class="form-control-file" name="file" aria-describedby="fileHelp">
+          <small id="fileHelp" class="form-text text-muted">Input file to import.</small>
+        </div>
+        <button type="submit" name="preview" class="btn btn-success btn-sm">
+          <span class="glyphicon glyphicon-eye-open"></span> Import
+        </button>
+      </form>
+    </div>
+    <div class="col-sm-4">
+    </div>
   </div>
-  <button type="submit" name="preview" class="btn btn-success btn-sm">
-    <span class="glyphicon glyphicon-eye-open"></span> Preview
-  </button>
-</form>
 </div>
 <br>
 <?php
@@ -50,12 +57,9 @@ $open = fopen($file,'r');
     <td><?php echo $ratio;?></td>
     <td><?php echo $ratio_b;?></td>
   </tr>
-<?php } ?>
-  <button name="import" type="button" class="btn btn-primary" action="<?php echo base_url('index.php/import/insertDB')?>">
-    <span class='glyphicon glyphicon-upload'></span> Import
-  </button>
-  <hr>
-<?php } ?>
+<?php
+  }
+} ?>
 </tbody>
 </table>
 <div class="alert alert-success">
